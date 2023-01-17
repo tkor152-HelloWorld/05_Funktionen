6cf6c3bcaf7ba84f6507dcbffd5f28e0ccb189d8
git4cc-noob <ktcc.0822@gmail.com>
2023-01-17 10:56:52 +0100

    Parametrisierung

--- script_01.js
+++ script_01.js
@@ -1,6 +1,25 @@
 "use strict";
 
 /***** Funktionen 01 *****/
-
 // 1. Kapselung von Codeblöcken
 
+// Funktionsaufruf call
+// test();
+
+// Funktionsrumpf (body) | callee
+// Funktionsdeklaration
+function test()
+{
+    console.log("Hallo Patrick!");
+}
+
+/*****Funktion 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() 
+{   
+    let firstName = 'Martin'
+    console.log("Hallo " + firstName + "!");
+}
