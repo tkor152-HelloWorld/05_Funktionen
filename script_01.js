661b1c6678b571294ee1519d6de4e1f92165f621
git4cc-noob <ktcc.0822@gmail.com>
2023-01-17 14:37:08 +0100

    Trennung der Funktionen

--- script_01.js
+++ script_01.js
@@ -33,15 +33,55 @@ function ausgabeNamen()
 
 function ausgabeNamenParam(firstName) // Funktion + Parameter
 {   
-    console.log("Hallo " + firstName + "!")
+    console.log("Hallo " + firstName + "!");
 }
+
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter
 
 // ausgabeNamenParams("Max", "Mütze")
-ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
 
 function ausgabeNamenParams(firstName, familyName)
  {
     console.log("Hallo " + firstName + " " + familyName + "!")
+}
+
+/***** Funktionen 03a *****/
+// 3a. Vorbereitung
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+// ausgabeNamenParams2("Max", "Mütze");
+
+function ausgabeNamenParams2(firstName, familyName)
+ {
+    // 1. Funktionalität: string composing
+    const gap = " ";
+    let outputStr = "Hallo" + gap + firstName + gap + familyName + "!";
+
+    // 2. string output
+
+    console.log(outputStr);
+}
+/***** Funktionen 03b *****/
+// 3b. Trennung der Funktionen | return
+
+// 1. Funktionalität: string composing
+// output(getString("Max","Mütze")); //call
+output(getString(prompt("Vorname?"),prompt("Nachname?")));
+
+function getString(firstName, familyName)
+{
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    return outputStr; // return sendet Daten an den call zurück
+    console.log("Funktion beendet!"); //return bricht die Funktion ab!
+}
+// 2. string output
+
+// output("Hi"); // call
+function output(outputData)
+{
+    console.log(outputData);
 }
